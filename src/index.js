b16e227e3bc9b3e7edcf8180750a4c6616ba7d9c
dielies <160782509+dielies@users.noreply.github.com>
2024-09-25 13:27:01 +0200

    Added .js form handling and Typewriter

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generateHikeSuggestion(event) {
+  event.preventDefault();
 
+  new Typewriter("#hike-suggestion", {
+    strings: ["Looking for suggestions"],
+    autoStart: true,
+    cursor: "",
+    delay: 50,
+  });
+}
+
+let hikeFormElement = document.querySelector("#hike-generator-form");
+hikeFormElement.addEventListener("submit", generateHikeSuggestion);
